1e90b6f6db94f90462f452e23b63e39236b1fadc
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2023-12-01 08:04:36 -0500

    Solve: day 1 part 1

--- src/main/java/aoc2023/Day1.java
+++ src/main/java/aoc2023/Day1.java
@@ -1,6 +1,8 @@
 package aoc2023;
 
 import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.LoggerFactory;
 
 import aoc.FileUtils;
@@ -14,7 +16,7 @@ import ch.qos.logback.classic.LoggerContext;
  * @author Paul Cormier
  *
  */
-public class Day1{
+public class Day1 {
 
     private static final Logger log = ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger(Day1.class);
 
@@ -30,14 +32,14 @@ public class Day1{
         List<String> testLines = FileUtils.readFile(TEST_INPUT_TXT);
         log.trace(testLines.toString());
 
-        log.info("{}", part1(testLines));
+        log.info("The sum of the values in the test input is: {}. It should be 142.", part1(testLines));
 
         log.setLevel(Level.INFO);
 
         // Read the real file
         List<String> lines = FileUtils.readFile(INPUT_TXT);
 
-        log.info("{}", part1(lines));
+        log.info("The sum of the values in the real input is: {}", part1(lines));
 
         // PART 2
 
@@ -50,9 +52,27 @@ public class Day1{
         log.info("{}", part2(lines));
     }
 
+    /**
+     * Given a list of lines of alphanumeric characters, find the first and last
+     * digits (could be the same), treat those as a two digit number, and sum
+     * the value for all lines.
+     * 
+     * @param lines
+     *            The lines of alphanumeric characters.
+     * @return The sum of interpreting the first and last digits of each line as
+     *         a number.
+     */
     private static int part1(final List<String> lines) {
 
-        return -1;
+        return lines.stream().mapToInt(Day1::findValue).sum();
+    }
+
+    private static int findValue(String line) {
+        String digits = StringUtils.getDigits(line);
+        char firstDigit = digits.charAt(0);
+        char secondDigit = digits.charAt(digits.length()-1);
+
+        return (firstDigit - '0') * 10 + (secondDigit - '0');
     }
 
     private static int part2(final List<String> lines) {
