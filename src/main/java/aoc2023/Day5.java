39d43a95fa2b983318b9ef633ccff00cfa8bb996
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2023-12-05 15:59:43 -0500

    Attempt 1: day 5 part 2
    Doesn't account for remaining chunks of ranges.

--- src/main/java/aoc2023/Day5.java
+++ src/main/java/aoc2023/Day5.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.apache.commons.lang3.Range;
@@ -50,6 +51,11 @@ public class Day5 {
             return idRange.contains(id) ? Optional.of(id + translationValue) : Optional.empty();
         }
 
+        @Override
+        public String toString() {
+            return String.format("%s, translationValue=%s]", idRange, translationValue);
+        }
+
     }
 
     public static void main(String[] args) {
@@ -74,11 +80,11 @@ public class Day5 {
         log.info("Part 2:");
         log.setLevel(Level.DEBUG);
 
-        log.info("{}", part2(testLines));
+        log.info("The lowest location number is {} (should be 46)", part2(testLines));
 
         log.setLevel(Level.INFO);
 
-        log.info("{}", part2(lines));
+        log.info("The lowest location number is {}", part2(lines));
     }
 
     /**
@@ -131,7 +137,7 @@ public class Day5 {
 
             // Start new maps
             if (line.matches("^\\D.*")) {
-                log.debug("Starting {}", line);
+                log.trace("Starting {}", line);
                 currentMap = new ArrayList<>();
                 almanac.add(currentMap);
                 continue;
@@ -148,9 +154,67 @@ public class Day5 {
         return almanac;
     }
 
-    private static int part2(final List<String> lines) {
+    private static long part2(final List<String> lines) {
 
-        return -1;
+        // Seeds: the first line is ranges of seeds
+        List<Long> inputNumbers = Stream.of(lines.get(0).substring(7).split(" "))
+                                        .map(Long::valueOf)
+                                        .collect(Collectors.toList());
+
+        List<Range<Long>> seeds = IntStream.range(0, inputNumbers.size() / 2)
+                                           .mapToObj(i -> {
+                                               Long start = inputNumbers.get(i * 2);
+                                               Long end = start + inputNumbers.get(i * 2 + 1) - 1;
+                                               return Range.between(start, end);
+                                           })
+                                           .collect(Collectors.toList());
+
+        log.debug("Seeds: {}", seeds);
+
+        // Parse lines into almanac list
+        List<List<AlmanacMap>> almanacMaps = parseAlmanac(lines.subList(2, lines.size()));
+        log.debug("Almanac:\n{}", almanacMaps.stream().map(List::toString).collect(Collectors.joining("\n")));
+
+        // Translate all seed ranges, and find lowest value
+        long minValue = Long.MAX_VALUE;
+        for (Range<Long> seedRange : seeds) {
+            log.debug("For seed range: {}", seedRange);
+            // Compare this range with the ranges for each stage of the Almanac
+            List<Range<Long>> nextRanges = new ArrayList<>();
+            nextRanges.add(seedRange);
+
+            for (List<AlmanacMap> stage : almanacMaps) {
+                List<Range<Long>> currentRanges = new ArrayList<>(nextRanges);
+//                nextRanges.clear();
+
+                for (Range<Long> currentRange : currentRanges) {
+                    for (AlmanacMap map : stage) {
+                        // Split the ranges if there's overlap, and translate to the new range
+                        if (currentRange.isOverlappedBy(map.idRange)) {
+                            // Remove the current range
+                            nextRanges.remove(currentRange);
+                            // Translate and add the overlap
+                            Range<Long> overlap = currentRange.intersectionWith(map.idRange);
+                            nextRanges.add(Range.between(map.mapId(overlap.getMinimum()).get(), map.mapId(overlap.getMaximum()).get()));
+                            // Remove the overlap from the current range, and add the remainders back to the current queue
+                            if (currentRange.getMinimum() < map.idRange.getMinimum())
+                                nextRanges.add(Range.between(currentRange.getMinimum(), map.idRange.getMinimum() - 1));
+                            if (currentRange.getMaximum() > map.idRange.getMaximum())
+                                nextRanges.add(Range.between(map.idRange.getMaximum() + 1, currentRange.getMaximum()));
+                        }
+
+                    }
+                }
+                log.debug("Translated to: {}", nextRanges);
+            }
+
+            long minValueForSeed = nextRanges.stream().mapToLong(Range::getMinimum).min().getAsLong();
+            log.debug("Min value: {}", minValueForSeed);
+
+            minValue = Math.min(minValueForSeed, minValue);
+        }
+
+        return minValue;
     }
 
 }
