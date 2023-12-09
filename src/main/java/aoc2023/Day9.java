f250f240e19be4c30aae0641d1ff71e7aa729ada
Paul Cormier <lastloginname@gmail.com>
2023-12-09 09:33:56 -0500

    Solve: day 9 part 1

---
+++ src/main/java/aoc2023/Day9.java
@@ -0,0 +1,101 @@
+package aoc2023;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.LoggerFactory;
+
+import aoc.FileUtils;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+
+/**
+ * https://adventofcode.com/2023/day/9
+ * 
+ * @author Paul Cormier
+ *
+ */
+public class Day9 {
+
+    private static final Logger log = ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger(Day9.class);
+
+    private static final String INPUT_TXT = "input/Day9.txt";
+
+    private static final String TEST_INPUT_TXT = "testInput/Day9.txt";
+
+    public static void main(String[] args) {
+
+        log.info("Part 1:");
+        log.setLevel(Level.DEBUG);
+
+        // Read the test file
+        List<String> testLines = FileUtils.readFile(TEST_INPUT_TXT);
+        log.trace("{}", testLines);
+
+        log.info("The sum of the extrapolated values is: {} (should be 114)", part1(testLines));
+
+        log.setLevel(Level.INFO);
+
+        // Read the real file
+        List<String> lines = FileUtils.readFile(INPUT_TXT);
+
+        log.info("The sum of the extrapolated values is: {}", part1(lines));
+
+        // PART 2
+        log.info("Part 2:");
+        log.setLevel(Level.DEBUG);
+
+        log.info("{}", part2(testLines));
+
+        log.setLevel(Level.INFO);
+
+        log.info("{}", part2(lines));
+    }
+
+    /**
+     * What is the sum of these extrapolated values?
+     * 
+     * @param lines The sequences of values for which the next digits need to be
+     *     estimated.
+     * @return The sum of the extrapolated values for each sequence.
+     */
+    private static int part1(final List<String> lines) {
+        return lines.stream()
+                    .map(l -> Arrays.asList(l.split(" "))
+                                    .stream()
+                                    .mapToInt(Integer::valueOf).toArray())
+                    .mapToInt(Day9::findNextValue)
+                    .sum();
+    }
+
+    private static int findNextValue(int[] sequence) {
+        log.debug("{}", Arrays.toString(sequence));
+
+        int nextValue = 0;
+        boolean allZero = false;
+        while (!allZero) {
+            allZero = true;
+            // Accumulate the last value of the sequence
+            nextValue += sequence[sequence.length - 1];
+
+            // Find the differences
+            int[] nextSequence = new int[sequence.length - 1];
+            for (int i = 0; i < nextSequence.length; i++) {
+                nextSequence[i] = sequence[i + 1] - sequence[i];
+                allZero &= nextSequence[i] == 0;
+            }
+
+            sequence = nextSequence;
+            log.debug("{}", Arrays.toString(sequence));
+        }
+
+        return nextValue;
+    }
+
+    private static int part2(final List<String> lines) {
+
+        return -1;
+    }
+
+}
