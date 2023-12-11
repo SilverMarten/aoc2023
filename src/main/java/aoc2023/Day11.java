290805216a6abf44750d29eb0d557e360843838d
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2023-12-11 08:56:20 -0500

    Solve: day 11 part 1

---
+++ src/main/java/aoc2023/Day11.java
@@ -0,0 +1,186 @@
+package aoc2023;
+
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.apache.commons.collections4.IterableUtils;
+import org.slf4j.LoggerFactory;
+
+import aoc.Coordinate;
+import aoc.FileUtils;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+
+/**
+ * https://adventofcode.com/2023/day/11
+ * 
+ * @author Paul Cormier
+ *
+ */
+public class Day11 {
+
+    private static final Logger log = ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger(Day11.class);
+
+    private static final String INPUT_TXT = "input/Day11.txt";
+
+    private static final String TEST_INPUT_TXT = "testInput/Day11.txt";
+
+    public static void main(String[] args) {
+
+        log.info("Part 1:");
+        log.setLevel(Level.DEBUG);
+
+        // Read the test file
+        List<String> testLines = FileUtils.readFile(TEST_INPUT_TXT);
+        log.trace("{}", testLines);
+
+        log.info("The sum of the shortest paths between the stars is: {} (should be 374)", part1(testLines));
+
+        log.setLevel(Level.INFO);
+
+        // Read the real file
+        List<String> lines = FileUtils.readFile(INPUT_TXT);
+
+        log.info("The sum of the shortest paths between the stars is: {}", part1(lines));
+
+        // PART 2
+        log.info("Part 2:");
+        log.setLevel(Level.DEBUG);
+
+        log.info("{}", part2(testLines));
+
+        log.setLevel(Level.INFO);
+
+        log.info("{}", part2(lines));
+    }
+
+    /**
+     * Expand the universe, then find the length of the shortest path between
+     * every pair of galaxies. What is the sum of these lengths?
+     * 
+     * @param lines
+     *            The lines representing the map of the stars.
+     * @return The sum of the shortest paths between each permutation of star
+     *         pairs.
+     */
+    private static int part1(final List<String> lines) {
+
+        // Find stars
+        AtomicInteger row = new AtomicInteger(0);
+        AtomicInteger column = new AtomicInteger(0);
+
+        Set<Coordinate> starMap = lines.stream()
+                                       .peek(l -> row.incrementAndGet())
+                                       .peek(l -> column.set(0))
+                                       .flatMap(l -> l.chars()
+                                                      .peek(c -> column.incrementAndGet())
+                                                      .filter(c -> c != '.')
+                                                      .mapToObj(c -> new Coordinate(row.get(), column.get())))
+                                       .collect(Collectors.toSet());
+
+        log.atDebug().setMessage("Star map:\n{}")
+           .addArgument(() -> printMap(starMap, row.get(), column.get()))
+           .log();
+
+        // Expand universe
+
+        // Find empty rows and columns
+        List<Integer> emptyRows = IntStream.rangeClosed(1, row.get()).boxed().collect(Collectors.toList());
+        List<Integer> emptyColumns = IntStream.rangeClosed(1, column.get()).boxed().collect(Collectors.toList());
+        starMap.forEach(s -> {
+            emptyRows.remove(Integer.valueOf(s.getRow()));
+            emptyColumns.remove(Integer.valueOf(s.getColumn()));
+        });
+
+        log.debug("Empty rows: {}", emptyRows);
+        log.debug("Empty columns: {}", emptyColumns);
+
+        // Move stars
+        Set<Coordinate> expandedStarMap = starMap.stream()
+                                                 .map(s -> new Coordinate(s.getRow() +
+                                                                          (int) IterableUtils.countMatches(emptyRows, r -> r < s.getRow()),
+                                                                          s.getColumn() + (int) IterableUtils.countMatches(emptyColumns,
+                                                                                                                           c -> c < s.getColumn())))
+                                                 .collect(Collectors.toSet());
+
+        log.atDebug().setMessage("Expanded star map:\n{}")
+           .addArgument(() -> printMap(expandedStarMap, row.get() + emptyRows.size(), column.get() + emptyColumns.size()))
+           .log();
+
+        // Compute paths
+        Queue<Coordinate> starsToCheck = new ArrayDeque<>(expandedStarMap);
+
+        int sumOfDistances = 0;
+        while (!starsToCheck.isEmpty()) {
+            Coordinate star = starsToCheck.poll();
+            //            sumOfDistances += starsToCheck.stream().mapToInt(s -> manhattanDistanceBetween(s, star)).sum();
+            for (Coordinate star2 : starsToCheck) {
+                log.atTrace()
+                   .setMessage("Distance between {} and {}: {}")
+                   .addArgument(star)
+                   .addArgument(star2)
+                   .addArgument(() -> manhattanDistanceBetween(star, star2))
+                   .log();
+                sumOfDistances += manhattanDistanceBetween(star, star2);
+            }
+        }
+
+        return sumOfDistances;
+    }
+
+    private static int part2(final List<String> lines) {
+
+        return -1;
+    }
+
+    /**
+     * Find the Manhattan distance between two points.
+     * 
+     * @param point1
+     *            A point to compare with the other point.
+     * @param point2
+     *            A point to compare with the other point.
+     * @return The sum of the difference between the rows and columns of the two
+     *         points.
+     */
+    private static int manhattanDistanceBetween(Coordinate point1, Coordinate point2) {
+        return Math.abs(point1.getRow() - point2.getRow()) + Math.abs(point1.getColumn() - point2.getColumn());
+    }
+
+    /**
+     * Create a printout of the map.
+     * 
+     * @param coordinates
+     *            The set of coordinates to display.
+     * @param rows
+     *            The number of rows in the map.
+     * @param columns
+     *            The number of columns in the map.
+     * @return A string representation of the map.
+     */
+    private static String printMap(Set<Coordinate> coordinates, int rows, int columns) {
+
+        int location = columns;
+
+        StringBuilder printout = new StringBuilder(rows * columns + rows);
+
+        while (location < (rows + 1) * columns) {
+            printout.append(coordinates.contains(new Coordinate(location / columns, location % columns + 1)) ? "*"
+                                                                                                             : ".");
+
+            if (location % columns == columns - 1)
+                printout.append('\n');
+
+            location++;
+        }
+
+        return printout.toString();
+    }
+
+}
