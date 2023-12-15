5308ebb78aaa054e7d6e08eab1801f726c1ffbaa
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2023-12-15 13:57:20 -0500

    Solve: day 15 part 2

--- src/main/java/aoc/_2023/Day15.java
+++ src/main/java/aoc/_2023/Day15.java
@@ -1,8 +1,15 @@
 package aoc._2023;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
+import org.apache.commons.collections4.IterableUtils;
 import org.slf4j.LoggerFactory;
 
 import aoc.FileUtils;
@@ -23,6 +30,23 @@ public class Day15 {
     private static final String INPUT_TXT = "input/Day15.txt";
 
     private static final String TEST_INPUT_TXT = "testInput/Day15.txt";
+
+    private static final class Lens {
+        String label;
+        int focalLength;
+
+        public Lens(String label, int focalLength) {
+            super();
+            this.label = label;
+            this.focalLength = focalLength;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("[%s %s]", label, focalLength);
+        }
+
+    }
 
     public static void main(String[] args) {
 
@@ -46,11 +70,13 @@ public class Day15 {
         log.info("Part 2:");
         log.setLevel(Level.DEBUG);
 
-        log.info("{}", part2(testLines));
+        log.info("The focusing power of the resulting lens configuration is: {} (should be 145)",
+                 part2(Arrays.asList(testLines.get(0).split(","))));
 
         log.setLevel(Level.INFO);
 
-        log.info("{}", part2(lines));
+        log.info("The focusing power of the resulting lens configuration is: {}",
+                 part2(Arrays.asList(lines.get(0).split(","))));
     }
 
     /**
@@ -65,13 +91,76 @@ public class Day15 {
     private static int part1(final String line) {
 
         return Stream.of(line.split(","))
-                     .mapToInt(step -> step.chars().reduce(0, (sum, character) -> (sum + character) * 17 % 256))
+                     .mapToInt(Day15::hash)
                      .sum();
     }
 
-    private static int part2(final List<String> lines) {
+    /**
+     * Follow the initialization sequence. What is the focusing power of the
+     * resulting lens configuration?
+     * 
+     * @param steps
+     *            The steps in the initialization sequence.
+     * @return The focusing power of the resulting lens configuration.
+     */
+    private static int part2(final List<String> steps) {
 
-        return -1;
+        Map<Integer, List<Lens>> boxMap = new HashMap<>();
+
+        // Perform the steps
+        steps.stream()
+             .forEach(step -> {
+                 String label = step.split("[-=]")[0];
+                 char operation = step.indexOf('=') > 0 ? '=' : '-';
+                 int value = step.charAt(step.length() - 1) - '0';
+                 int box = hash(label);
+                 List<Lens> boxContents = boxMap.computeIfAbsent(box, ArrayList::new);
+                 if (operation == '-') {
+                     boxContents.removeIf(l -> label.equals(l.label));
+                 } else {
+                     Optional.ofNullable(IterableUtils.find(boxContents, l -> label.equals(l.label)))
+                             .ifPresentOrElse(l -> l.focalLength = value,
+                                              () -> boxContents.add(new Lens(label, value)));
+                 }
+
+                 log.debug("After \"{}\":\n{}", step, boxMap);
+             });
+
+        // Add up focusing power
+
+        return boxMap.entrySet().stream()
+                     .mapToInt(b -> {
+                         AtomicInteger i = new AtomicInteger(1);
+                         return (b.getKey() + 1) * b.getValue().stream().mapToInt(l -> i.getAndIncrement() * l.focalLength).sum();
+                     })
+                     .sum();
+
+    }
+
+    /**
+     * The HASH algorithm is a way to turn any string of characters into a
+     * single number in the range 0 to 255. To run the HASH algorithm on a
+     * string, start with a current value of 0. Then, for each character in the
+     * string starting from the beginning:
+     * <ul>
+     * <li>Determine the ASCII code for the current character of the
+     * string.</li>
+     * <li>Increase the current value by the ASCII code you just
+     * determined.</li>
+     * <li>Set the current value to itself multiplied by 17.</li>
+     * <li>Set the current value to the remainder of dividing itself by
+     * 256.</li>
+     * </ul>
+     * 
+     * After following these steps for each character in the string in order,
+     * the current value is the output of the HASH algorithm.
+     * 
+     * @param label
+     *            The value being hashed
+     * @return The result of the HASH algorithm.
+     */
+    private static int hash(String label) {
+        return label.chars().reduce(0, (sum, character) -> (sum + character) * 17 % 256);
     }
 
 }
