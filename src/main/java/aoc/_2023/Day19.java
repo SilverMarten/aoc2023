75d6a1751464a133ecb8b0eaaf782b3d78e8f796
Paul Cormier <Paul.E.Cormier@rcmp-grc.gc.ca>
2023-12-19 17:00:57 -0500

    WIP: day 19 part 2

--- src/main/java/aoc/_2023/Day19.java
+++ src/main/java/aoc/_2023/Day19.java
@@ -1,17 +1,25 @@
 package aoc._2023;
 
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Queue;
 import java.util.Set;
+import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
+import java.util.function.ToLongFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.commons.lang3.Range;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.LoggerFactory;
 
@@ -33,6 +41,76 @@ public class Day19 {
     private static final String INPUT_TXT = "input/Day19.txt";
 
     private static final String TEST_INPUT_TXT = "testInput/Day19.txt";
+
+    private static final class RangedPart {
+
+        private static final int RANGE_MIN = 1;
+        private static final int RANGE_MAX = 4000;
+        private static final Range<Integer> DEFAULT_RANGE = Range.of(RANGE_MIN, RANGE_MAX);
+
+        Set<Range<Integer>> xRanges;
+        Set<Range<Integer>> mRanges;
+        Set<Range<Integer>> aRanges;
+        Set<Range<Integer>> sRanges;
+
+        public RangedPart() {
+            this.xRanges = Set.of(DEFAULT_RANGE);
+            this.mRanges = Set.of(DEFAULT_RANGE);
+            this.aRanges = Set.of(DEFAULT_RANGE);
+            this.sRanges = Set.of(DEFAULT_RANGE);
+        }
+
+        private RangedPart(RangedPart otherPart) {
+            this.xRanges = Set.copyOf(otherPart.xRanges);
+            this.mRanges = Set.copyOf(otherPart.mRanges);
+            this.aRanges = Set.copyOf(otherPart.aRanges);
+            this.sRanges = Set.copyOf(otherPart.sRanges);
+        }
+
+        public long getCombinations() {
+            ToLongFunction<Range<Integer>> mapper = r -> r.getMaximum() - r.getMinimum() + 1;
+            return xRanges.stream().mapToLong(mapper).sum() *
+                   aRanges.stream().mapToLong(mapper).sum() *
+                   mRanges.stream().mapToLong(mapper).sum() *
+                   sRanges.stream().mapToLong(mapper).sum();
+        }
+
+        public RangedPart evaluate(Workflow.Rule rule) {
+            RangedPart newPart = new RangedPart(this);
+            // Evaluate the rule, replacing the appropriate range with a (potentially) altered range
+
+            newPart.xRanges = newPart.xRanges.stream()
+                                             .map(range -> mapper(rule, range))
+                                             .collect(Collectors.toSet());
+            return newPart;
+        }
+
+        private static Range<Integer> mapper(Workflow.Rule rule, Range<Integer> range) {
+            return range.intersectionWith(">".equals(rule.comparisonString) ? Range.of(rule.value + 1,
+                                                                                       RANGE_MAX)
+                                                                            : Range.of(RANGE_MIN,
+                                                                                       rule.value - 1));
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(aRanges, mRanges, sRanges, xRanges);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            RangedPart other = (RangedPart) obj;
+            return Objects.equals(aRanges, other.aRanges) && Objects.equals(mRanges, other.mRanges) &&
+                   Objects.equals(sRanges, other.sRanges) && Objects.equals(xRanges, other.xRanges);
+        }
+
+    }
 
     private static final class Part {
         /** Extremely cool looking */
@@ -147,6 +225,23 @@ public class Day19 {
         public String toString() {
             return String.format("%s%s", name, rules);
         }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, rules);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Workflow other = (Workflow) obj;
+            return Objects.equals(name, other.name) && Objects.equals(rules, other.rules);
+        }
     }
 
     public static void main(String[] args) {
@@ -170,11 +265,14 @@ public class Day19 {
         log.info("Part 2:");
         log.setLevel(Level.DEBUG);
 
-        log.info("{}", part2(testLines));
+        long part2Result = part2(testLines);
+        log.info("The number of distinct combinations that will be accepted is: {} (should be 167409079868000)", part2Result);
+        if (part2Result != 167409079868000L)
+            log.error("The sample data is not adding up.");
 
         log.setLevel(Level.INFO);
 
-        log.info("{}", part2(lines));
+        log.info("The number of distinct combinations that will be accepted is: {}", part2(lines));
     }
 
     /**
@@ -189,29 +287,9 @@ public class Day19 {
     private static int part1(final List<String> lines) {
 
         // Parse the workflows
-        Map<String, Workflow> workflowMap = new HashMap<>();
-        int partsStart = 0;
-        for (String line : lines) {
-            partsStart++;
-            if (line.isBlank())
-                break;
+        Map<String, Workflow> workflowMap = parseWorkflows(lines);
 
-            // Find the workflow to add rules to
-            Workflow newWorkflow = workflowMap.computeIfAbsent(line.substring(0, line.indexOf('{')), Workflow::new);
-            // Parse the rules
-            newWorkflow.rules.addAll(Stream.of(line.substring(line.indexOf('{') + 1, line.length() - 1).split(","))
-                                           .map(r -> {
-                                               String conditionString = r.indexOf(':') >= 0 ? r.split(":")[0] : "";
-                                               String outcomeName = r.indexOf(':') >= 0 ? r.split(":")[1] : r;
-                                               return Workflow.Rule.from(conditionString,
-                                                                         workflowMap.computeIfAbsent(outcomeName, Workflow::new));
-                                           })
-                                           .collect(Collectors.toList()));
-
-        }
-
-        log.debug("Workflows:\n{}", workflowMap.values());
-
+        int partsStart = workflowMap.size() - 1;
         // Parse the parts 
         Set<Part> parts = lines.stream()
                                .skip(partsStart)
@@ -250,9 +328,92 @@ public class Day19 {
         return accepted.stream().mapToInt(Part::getSum).sum();
     }
 
-    private static int part2(final List<String> lines) {
+    /**
+     * Each of the four ratings (x, m, a, s) can have an integer value ranging
+     * from a minimum of 1 to a maximum of 4000. Of all possible distinct
+     * combinations of ratings, your job is to figure out which ones will be
+     * accepted.
+     * 
+     * How many distinct combinations of ratings will be accepted by the Elves'
+     * workflows?
+     * 
+     * @param lines
+     *            The lines representing workflows and parts (though the parts
+     *            are to be excluded).
+     * @return The number of distinct combinations that will be accepted.
+     */
+    private static long part2(final List<String> lines) {
 
-        return -1;
+        // Parse the workflows
+        Map<String, Workflow> workflowMap = parseWorkflows(lines);
+
+        // The workflows form a tree, with accept and reject at the leaves
+        // Traverse the tree and narrow down the ranges of values at each node
+        // The total possible combinations is the sum of the products of the ranges
+
+        // Start from "in"
+        Workflow start = workflowMap.get("in");
+        Workflow accept = workflowMap.get("A");
+        Workflow reject = workflowMap.get("R");
+
+        Set<RangedPart> acceptedParts = new HashSet<>();
+
+        Queue<Entry<Workflow, RangedPart>> stateQueue = new ArrayDeque<>();
+        stateQueue.add(new SimpleEntry<>(start, new RangedPart()));
+        while (!stateQueue.isEmpty()) {
+            Entry<Workflow, RangedPart> currentState = stateQueue.poll();
+            Workflow currentWorkflow = currentState.getKey();
+            RangedPart currentPart = currentState.getValue();
+
+            // If this is the accepted state, collect the ranged part that made it here
+            if (accept.equals(currentWorkflow)) {
+                acceptedParts.add(currentPart);
+                continue;
+            }
+            // If it's not the rejected state, process it accordingly
+            if (!reject.equals(currentWorkflow)) {
+                // Evaluate each rule and add those states to the queue
+                stateQueue.addAll(currentWorkflow.rules.stream()
+                                                       .map(r -> new SimpleEntry<>(r.outcome, currentPart.evaluate(r)))
+                                                       .collect(Collectors.toList()));
+            }
+        }
+
+        log.debug("{} accepted ranged parts.", acceptedParts.size());
+
+        return acceptedParts.stream().mapToLong(RangedPart::getCombinations).sum();
+    }
+
+    /**
+     * Read the lines representing {@link Workflow}s and construct the map of
+     * them all.
+     * 
+     * @param lines
+     *            The lines representing {@link Workflow}s.
+     * @return A map of {@link Workflow} names to their instances.
+     */
+    private static Map<String, Workflow> parseWorkflows(final List<String> lines) {
+        Map<String, Workflow> workflowMap = new HashMap<>();
+        for (String line : lines) {
+            if (line.isBlank())
+                break;
+
+            // Find the workflow to add rules to
+            Workflow newWorkflow = workflowMap.computeIfAbsent(line.substring(0, line.indexOf('{')), Workflow::new);
+            // Parse the rules
+            newWorkflow.rules.addAll(Stream.of(line.substring(line.indexOf('{') + 1, line.length() - 1).split(","))
+                                           .map(r -> {
+                                               String conditionString = r.indexOf(':') >= 0 ? r.split(":")[0] : "";
+                                               String outcomeName = r.indexOf(':') >= 0 ? r.split(":")[1] : r;
+                                               return Workflow.Rule.from(conditionString,
+                                                                         workflowMap.computeIfAbsent(outcomeName, Workflow::new));
+                                           })
+                                           .collect(Collectors.toList()));
+
+        }
+
+        log.debug("Workflows:\n{}", workflowMap.values());
+        return workflowMap;
     }
 
 }
