89f18da52807f692da1267a87377610f65e44c37
Paul Cormier <lastloginname@gmail.com>
2023-12-23 21:49:08 -0500

    Attempt 1: day 23 part 2
    Well, it works for the sample data, but takes way too long on the real
    data.
    
    Maybe I needed to shorten the graph by setting the vertexes' weights to
    the distance between intersections or something... Maybe that's still
    too many possibilities.

--- src/main/java/aoc/_2023/Day23.java
+++ src/main/java/aoc/_2023/Day23.java
@@ -105,16 +105,16 @@ public class Day23 {
         log.info("Part 2:");
         log.setLevel(Level.DEBUG);
 
-        expectedTestResult = 1_234_567_890;
+        expectedTestResult = 154;
         int part2TestResult = part2(testLines);
-        log.info("{} (should be {})", part2TestResult, expectedTestResult);
+        log.info("The longest path is: {} (should be {})", part2TestResult, expectedTestResult);
 
         if (part2TestResult != expectedTestResult)
             log.error("The test result doesn't match the expected value.");
 
         log.setLevel(Level.INFO);
 
-        log.info("{}", part2(lines));
+        log.info("The longest path is: {}", part2(lines));
     }
 
     /**
@@ -243,9 +243,104 @@ public class Day23 {
 
     }
 
+    /**
+     * Find the longest hike you can take through the surprisingly dry hiking trails listed on your map. How many steps
+     * long is the longest hike?
+     * 
+     * @param lines The lines describing the map.
+     * @return The longest path.
+     */
     private static int part2(final List<String> lines) {
 
-        return -1;
+        int rows = lines.size();
+        int columns = lines.get(0).length();
+
+        // Parse the map
+        Map<Coordinate, Node> nodeMap = parseNodes(lines);
+
+        log.atDebug()
+           .setMessage("Nodes:\n{}")
+           .addArgument(() -> Coordinate.printMap(1, 1, rows, columns, nodeMap, n -> n.type, '#'))
+           .log();
+
+        // Create a new graph
+        Graph<Node, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+
+        // First add all vertexes
+        nodeMap.values().forEach(graph::addVertex);
+
+        // Add edges
+        nodeMap.values().stream().forEach(node -> {
+            // Flat terrain checks all
+            Coordinate nodePosition = node.getPosition();
+            /*if (node.getType() == '.') {*/
+            nodePosition.findOrthogonalAdjacent()
+                        .stream()
+                        .filter(nodeMap::containsKey)
+                        .map(nodeMap::get)
+                        .forEach(neighbour -> {
+                            /*if (neighbour.getType() == '.') {*/
+                            node.getNeighbours().add(neighbour);
+                            // Bi-directional
+                            graph.addEdge(node, neighbour);
+                            //                            graph.addEdge(neighbour, node);
+                            /* } else {
+                                 // If it's a slope it can't be reached from its direction
+                                 Coordinate neighbourDirection = Direction.withSymbol(neighbour.getType())
+                                                                          .getTranslation();
+                                 Coordinate neighbourPosition = neighbour.getPosition();
+                                 if (!nodePosition.equals(Coordinate.of(neighbourPosition.getRow()
+                                                                        + neighbourDirection.getRow(),
+                                                                        neighbourPosition.getColumn() + neighbourDirection.getColumn()))) {
+                                     node.getNeighbours().add(neighbour);
+                                     graph.addEdge(node, neighbour);
+                                 }
+                            
+                                 // If it's sloped away, there's no return path
+                                 if (!neighbourPosition.equals(Coordinate.of(nodePosition.getRow()
+                                                                             + neighbourDirection.getRow(),
+                                                                             nodePosition.getColumn() + neighbourDirection.getColumn())))
+                                     graph.addEdge(neighbour, node);
+                             }*/
+                        });
+            /*} else {
+                // Sloped terrain always (only) connects to its neighbour in its direction
+                Coordinate neighbourDirection = Direction.withSymbol(node.getType()).getTranslation();
+                Coordinate neighbourPosition = Coordinate.of(nodePosition.getRow() + neighbourDirection.getRow(),
+                                                             nodePosition.getColumn() + neighbourDirection.getColumn());
+                if (nodeMap.containsKey(neighbourPosition)) {
+                    Node neighbour = nodeMap.get(neighbourPosition);
+                    // Make sure it's not two slopes facing each other
+                    char neighbourType = neighbour.getType();
+                    if (!(neighbourType != '.'
+                          && node.getType() == Direction.withSymbol(neighbourType).opposite().getSymbol())) {
+                        node.getNeighbours().add(neighbour);
+                        graph.addEdge(node, neighbour);
+                    }
+                }
+            }*/
+        });
+
+        if (log.isTraceEnabled()) {
+            outputGraph(graph);
+        }
+
+        Node start = nodeMap.get(Coordinate.of(1, 2));
+        Node end = nodeMap.get(Coordinate.of(rows, columns - 1));
+
+        AllDirectedPaths<Node, DefaultEdge> algorithm = new AllDirectedPaths<>(graph);
+        List<GraphPath<Node, DefaultEdge>> paths = algorithm.getAllPaths(start, end, true, nodeMap.size());
+
+        List<Node> path = paths.stream()
+                               .sorted(Comparator.comparingInt((GraphPath<Node, DefaultEdge> g) -> g.getLength())
+                                                 .reversed())
+                               .findFirst()
+                               .map(GraphPath::getVertexList)
+                               .orElseGet(Collections::emptyList);
+
+        log.debug("Path from {}, to {}:\n{}", start.getPosition(), path.get(path.size() - 1).getPosition(), path);
+
+        return path.size() - 1;
     }
 
 }
