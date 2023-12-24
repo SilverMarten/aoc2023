8a72de2fad2bba9250f149c2df5b91ae19aef567
Paul Cormier <lastloginname@gmail.com>
2023-12-23 21:28:46 -0500

    Solve: day 23 part 1
    Used JGraphT... Interesting tool.

--- src/main/java/aoc/_2023/Day23.java
+++ src/main/java/aoc/_2023/Day23.java
@@ -1,18 +1,31 @@
 package aoc._2023;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import javax.imageio.ImageIO;
+
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.AllDirectedPaths;
+import org.jgrapht.ext.JGraphXAdapter;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
 import org.slf4j.LoggerFactory;
+
+import com.mxgraph.layout.mxIGraphLayout;
+import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
+import com.mxgraph.util.mxCellRenderer;
 
 import aoc.Coordinate;
 import aoc.Direction;
@@ -39,12 +52,12 @@ public class Day23 {
         private final Coordinate position;
         private final char type;
 
-        private final Map<Node, Integer> costMap;
+        private final Set<Node> neighbours;
 
         public Node(Coordinate position, char type) {
             this.position = position;
             this.type = type;
-            this.costMap = new HashMap<>();
+            this.neighbours = new HashSet<>();
         }
 
         public Coordinate getPosition() {
@@ -55,8 +68,8 @@ public class Day23 {
             return type;
         }
 
-        public Map<Node, Integer> getCostMap() {
-            return costMap;
+        public Set<Node> getNeighbours() {
+            return neighbours;
         }
 
         @Override
@@ -124,18 +137,27 @@ public class Day23 {
            .addArgument(() -> Coordinate.printMap(1, 1, rows, columns, nodeMap, n -> n.type, '#'))
            .log();
 
-        // Connect the nodes
+        // Create a new graph
+        Graph<Node, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+
+        // First add all vertexes
+        nodeMap.values().forEach(graph::addVertex);
+
+        // Add edges
         nodeMap.values().stream().forEach(node -> {
             // Flat terrain checks all
             Coordinate nodePosition = node.getPosition();
-            if (node.getType() == '.')
+            if (node.getType() == '.') {
                 nodePosition.findOrthogonalAdjacent()
                             .stream()
                             .filter(nodeMap::containsKey)
                             .map(nodeMap::get)
                             .forEach(neighbour -> {
                                 if (neighbour.getType() == '.') {
-                                    node.getCostMap().put(neighbour, 0);
+                                    node.getNeighbours().add(neighbour);
+                                    // Bi-directional
+                                    graph.addEdge(node, neighbour);
+                                    graph.addEdge(neighbour, node);
                                 } else {
                                     // If it's a slope it can't be reached from its direction
                                     Coordinate neighbourDirection = Direction.withSymbol(neighbour.getType())
@@ -143,12 +165,20 @@ public class Day23 {
                                     Coordinate neighbourPosition = neighbour.getPosition();
                                     if (!nodePosition.equals(Coordinate.of(neighbourPosition.getRow()
                                                                            + neighbourDirection.getRow(),
-                                                                           neighbourPosition.getColumn() + neighbourDirection.getColumn())))
-                                        node.getCostMap().put(neighbour, 0);
+                                                                           neighbourPosition.getColumn() + neighbourDirection.getColumn()))) {
+                                        node.getNeighbours().add(neighbour);
+                                        graph.addEdge(node, neighbour);
+                                    }
+
+                                    // If it's sloped away, there's no return path
+                                    if (!neighbourPosition.equals(Coordinate.of(nodePosition.getRow()
+                                                                                + neighbourDirection.getRow(),
+                                                                                nodePosition.getColumn() + neighbourDirection.getColumn())))
+                                        graph.addEdge(neighbour, node);
                                 }
                             });
-            else {
-                // Sloped terrain always connects to its neighbour in its direction
+            } else {
+                // Sloped terrain always (only) connects to its neighbour in its direction
                 Coordinate neighbourDirection = Direction.withSymbol(node.getType()).getTranslation();
                 Coordinate neighbourPosition = Coordinate.of(nodePosition.getRow() + neighbourDirection.getRow(),
                                                              nodePosition.getColumn() + neighbourDirection.getColumn());
@@ -157,68 +187,50 @@ public class Day23 {
                     // Make sure it's not two slopes facing each other
                     char neighbourType = neighbour.getType();
                     if (!(neighbourType != '.'
-                          && node.getType() == Direction.withSymbol(neighbourType).opposite().getSymbol()))
-                        node.getCostMap().put(neighbour, 0);
+                          && node.getType() == Direction.withSymbol(neighbourType).opposite().getSymbol())) {
+                        node.getNeighbours().add(neighbour);
+                        graph.addEdge(node, neighbour);
+                    }
                 }
             }
         });
 
-        // Determine costs
-        Map<Node, Integer> maxCostMap = new HashMap<>();
-        Node end = nodeMap.get(Coordinate.of(rows, columns-1));
-        maxCostMap.put(end, 0);
+        if (log.isTraceEnabled()) {
+            outputGraph(graph);
+        }
+
         Node start = nodeMap.get(Coordinate.of(1, 2));
-        Stack<Node> nodesToCheck = new Stack<>();
-        Queue<Node> nextNodes = new ArrayDeque<>();
-        // Build the paths
-        nextNodes.add(start);
-        while (!nextNodes.isEmpty()) {
-            Node currentNode = nextNodes.poll();
-            
-            if (!nodesToCheck.contains(currentNode) && !maxCostMap.containsKey(currentNode)) {
-                nodesToCheck.add(currentNode);
-            }
-            nextNodes.addAll(currentNode.costMap.keySet()
-                                                .stream()
-                                                .filter(n -> !nodesToCheck.contains(n))
-                                                .collect(Collectors.toList()));
-        }
+        Node end = nodeMap.get(Coordinate.of(rows, columns - 1));
 
-        log.debug("Path to check:\n{}", nodesToCheck);
+        AllDirectedPaths<Node, DefaultEdge> algorithm = new AllDirectedPaths<Node, DefaultEdge>(graph);
+        List<GraphPath<Node, DefaultEdge>> paths = algorithm.getAllPaths(start, end, true, nodeMap.size());
 
-        // Start from the end
-        while (!nodesToCheck.isEmpty()) {
-            Node currentNode = nodesToCheck.pop();
-            int maxCost = currentNode.getCostMap()
-                                     .entrySet()
-                                     .stream()
-                                     .filter(e -> !nodesToCheck.contains(e.getKey()))
-                                     .mapToInt(Entry::getValue)
-                                     .max()
-                                     .orElse(0);
-            maxCostMap.put(currentNode, Math.max(maxCostMap.getOrDefault(currentNode, 0), maxCost + 1));
-        }
-
-        log.debug("Max cost map:\n{}", maxCostMap);
-
-        // Find the longest path
-        List<Node> path = new ArrayList<>();
-        Node next = start;
-        while (next != null) {
-            path.add(next);
-            next = next.getCostMap()
-                       .keySet()
-                       .stream()
-                       .filter(n -> !path.contains(n))
-                       .sorted(Comparator.comparing(maxCostMap::get))
-                       .sorted((a, b) -> -1)
-                       .findFirst()
-                       .orElse(null);
-        }
+        List<Node> path = paths.stream()
+                               .sorted(Comparator.comparingInt((GraphPath<Node, DefaultEdge> g) -> g.getLength())
+                                                 .reversed())
+                               .findFirst()
+                               .map(GraphPath::getVertexList)
+                               .orElseGet(Collections::emptyList);
 
         log.debug("Path from {}, to {}:\n{}", start.getPosition(), path.get(path.size() - 1).getPosition(), path);
 
-        return path.size();
+        return path.size() - 1;
+    }
+
+    private static void outputGraph(Graph<Node, DefaultEdge> graph) {
+        File imageFile = new File("out/Day23-graph.png");
+        try {
+            imageFile.createNewFile();
+            JGraphXAdapter<Node, DefaultEdge> graphAdapter = new JGraphXAdapter<Node, DefaultEdge>(graph);
+            mxIGraphLayout layout = new mxHierarchicalLayout(graphAdapter);
+            layout.execute(graphAdapter.getDefaultParent());
+
+            BufferedImage image = mxCellRenderer.createBufferedImage(graphAdapter, null, 2, Color.WHITE, true, null);
+            ImageIO.write(image, "PNG", imageFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private static Map<Coordinate, Node> parseNodes(List<String> lines) {
